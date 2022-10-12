b6b90e55436d511f4f07758b51097f94d7206c79
Shubham <skpawar1305@gmail.com>
2022-10-12 19:34:49 +0200

    R2022a -> R2022b

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
+import os
 from glob import glob
 from setuptools import setup
 
 package_name = 'webots_spot'
-data_files = [
-    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    ('share/' + package_name + '/launch', glob('launch/*_launch.py')),
-    ('share/' + package_name + '/worlds', glob('worlds/*.wbt')),
-    ('share/' + package_name + '/resource', glob('resource/*')),
-    ('share/' + package_name + '/map', glob('map/*')),
-    ('share/' + package_name + '/params', glob('params/*.yaml')),
-    ('share/' + package_name + '/protos', glob('protos/*.proto')),
-    ('share/' + package_name + '/protos/icons', glob('protos/icons/*.png')),
-    ('share/' + package_name + '/protos/apriltags/protos', glob('apriltags/protos/*.proto')),
-    ('share/' + package_name + '/protos/apriltags/images', glob('apriltags/images/*.png')),
-]
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=data_files,
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*_launch.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.wbt')),
+        (os.path.join('share', package_name, 'resource'), glob('resource/*')),
+        (os.path.join('share', package_name, 'protos'), glob('protos/*.proto')),
+        (os.path.join('share', package_name, 'protos', 'icons'), glob('protos/icons/*')),
+        (os.path.join('share', package_name, 'protos', 'meshes'), glob('protos/meshes/*')),
+        (os.path.join('share', package_name, 'protos', 'textures'), glob('protos/textures/*')),
+        (os.path.join('share', package_name, 'protos', 'UR3e', 'meshes'), glob('protos/UR3e/meshes/*')),
+        (os.path.join('share', package_name, 'protos', 'UR3e', 'textures'), glob('protos/UR3e/textures/*')),
+        (os.path.join('share', package_name, 'protos', 'apriltags', 'protos'), glob('apriltags/protos/*.proto')),
+        (os.path.join('share', package_name, 'protos', 'apriltags', 'images'), glob('apriltags/images/*.png')),
+        (os.path.join('share', package_name, 'map'), glob('map/*')),
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='max',
