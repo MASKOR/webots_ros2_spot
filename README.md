4a29d53791b6c0a27c317e4181a7ac52330192e2
Shubham <skpawar1305@gmail.com>
2023-07-24 19:00:03 +0200

    update for R2023b
    also, update install instructions

--- README.md
+++ README.md
@@ -1,48 +1,48 @@
 # Webots ROS2 Spot
 
-This is a ROS 2 package to simulate the Boston Dynamics spot in [webots](https://cyberbotics.com/). Spot is able to walk around, to sit, standup and lie down. We also attached some sensors on spot, like a kincet and a 3D laser.
-The world contains apriltags, a red line to test lane follower and soon objects for manipulation tasks.
+This is a ROS 2 package to simulate the Boston Dynamics spot in [webots](https://cyberbotics.com/). Spot is able to walk around, to sit, standup and lie down. We also attached some sensors on spot, like a kinect and a 3D laser.
+The world contains apriltags, a red line to test lane follower and objects for manipulation tasks.
 
 ![spot_UR3](https://fh-aachen.sciebo.de/s/zDZLpVTjPWLzt7x/download)
 
 ## Prerequisites
 
     - Tested for ubuntu 22.04
-    - ROS 2 humble
-    - Webots R2022b
+    - ROS 2 Humble
+    - Webots R2023b
     - Webots ROS 2 interface
-
-[Webots R2022b](https://cyberbotics.com/doc/guide/installation-procedure#installing-the-debian-package-with-the-advanced-packaging-tool-apt) can be installed via apt package manager or downloaded [here](https://github.com/cyberbotics/webots/releases).
-
-The webots ros2 interface has to be installed from [sources](https://github.com/cyberbotics/webots_ros2/wiki/Complete-Installation-Guide#install-webots_ros2-from-sources).
 
 ## Install
 
 ### Install dependencies
-```
-sudo apt install ros-$ROS_DISTRO-webots* ros-$ROS_DISTRO-nav2* -y
-sudo apt install ros-$ROS_DISTRO-pointcloud-to-laserscan -y
-sudo apt install ros-$ROS_DISTRO-moveit* -y
-sudo apt install python3-open3d -y # for Ubuntu 22.04
-pip3 install scipy pupil-apriltags
-pip3 install open3d # for Ubuntu 20.04
-```
+
+    sudo apt install ros-$ROS_DISTRO-webots* ros-$ROS_DISTRO-nav2* -y
+    sudo apt install ros-$ROS_DISTRO-pointcloud-to-laserscan -y
+    sudo apt install ros-$ROS_DISTRO-moveit* -y
+    sudo apt install python3-open3d -y # for Ubuntu 22.04
+    pip3 install scipy pupil-apriltags
+    pip3 install open3d # for Ubuntu 20.04
 
 ### webots_ros2_spot
-```
-# Source ROS 2
-source /opt/ros/$ROS_DISTRO/local_setup.bash
 
-cd /path/to/ros2_ws
-git clone https://github.com/MASKOR/webots_ros2_spot src/webots_spot
+    # Source ROS2
+    . /opt/ros/$ROS_DISTRO/setup.bash
 
-# spot_msgs
-git clone https://github.com/MASKOR/webots_spot_msgs src/spot_msgs
+    mkdir ~/ros2_ws
+    cd ~/ros2_ws
 
-# Build everything
-colcon build --symlink-install
-source install/local_setup.bash
-```
+    # webots_ros2
+    git clone https://github.com/cyberbotics/webots_ros2 -b 2023.1.1 --recursive src/webots_ros2
+
+    # webots_spot
+    git clone https://github.com/MASKOR/webots_ros2_spot src/webots_spot
+
+    # webots_spot_msgs
+    git clone https://github.com/MASKOR/webots_spot_msgs src/webots_spot_msgs
+
+    # Build everything
+    colcon build --symlink-install
+    . install/setup.bash
 
 ## Start
 Starting the simulation:
--- launch/spot_launch.py
+++ launch/spot_launch.py
@@ -1,41 +1,32 @@
+#!/usr/bin/env python
+
 import os
 import pathlib
 import launch
 from launch import LaunchDescription
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch_ros.actions import Node
-from webots_ros2_driver.utils import controller_url_prefix
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
 
 package_dir = get_package_share_directory('webots_spot')
 
 
-def get_ros2_nodes(*args):
-    spot_driver = Node(
-        package='webots_ros2_driver',
-        executable='driver',
-        output='screen',
-        additional_env={'WEBOTS_CONTROLLER_URL': controller_url_prefix() + 'Spot'},
-        parameters=[
-            {'robot_description': pathlib.Path(os.path.join(package_dir, 'resource', 'spot.urdf')).read_text()},
-            {'use_sim_time': False},
-            {'set_robot_state_publisher': False}, # foot positions are wrong with webot's urdf
-        ],
-    )
-    return [
-        spot_driver,
-    ]
-
-
 def generate_launch_description():
-    def load_file(filename):
-        return pathlib.Path(os.path.join(package_dir, 'resource', filename)).read_text()
-
     webots = WebotsLauncher(
         world=PathJoinSubstitution([package_dir, 'worlds', 'spot.wbt'])
+    )
+
+    spot_driver = WebotsController(
+        robot_name='Spot',
+        parameters=[
+            {'robot_description': os.path.join(package_dir, 'resource', 'spot.urdf'),
+             'use_sim_time': False,
+             'set_robot_state_publisher': False}, # foot positions are wrong with webot's urdf
+        ],
+        respawn=True
     )
 
     robot_state_publisher = Node(
@@ -43,8 +34,8 @@ def generate_launch_description():
         executable='robot_state_publisher',
         output='screen',
         parameters=[
-            {'robot_description': load_file('rd_spot.urdf')},
-            {'use_sim_time': False},
+            {'robot_description': pathlib.Path(os.path.join(package_dir, 'resource', 'rd_spot.urdf')).read_text(),
+             'use_sim_time': False},
         ],
     )
 
@@ -60,39 +51,31 @@ def generate_launch_description():
             on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
         )
     )
-    ros2_supervisor = Ros2SupervisorLauncher()
-    # The following line is important!
-    # This event handler respawns the ROS 2 nodes on simulation reset (supervisor process ends).
-    reset_handler = launch.actions.RegisterEventHandler(
-        event_handler=launch.event_handlers.OnProcessExit(
-            target_action=ros2_supervisor,
-            on_exit=get_ros2_nodes,
-        )
+
+    pointcloud_to_laserscan_node = Node(
+        package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+        remappings=[('cloud_in', '/Spot/Velodyne_Puck/point_cloud'), ],
+        parameters=[{
+            'transform_tolerance': 0.01,
+            'min_height': 0.0,
+            'max_height': 1.0,
+            'angle_min': -3.14,
+            'angle_max': 3.14,
+            'angle_increment': 0.00872,
+            'scan_time': 0.1,
+            'range_min': 0.9,
+            'range_max': 100.0,
+            'use_inf': True,
+            'inf_epsilon': 1.0,
+        }],
+        name='pointcloud_to_laserscan'
     )
 
     return LaunchDescription([
         webots,
-        ros2_supervisor,
+        spot_driver,
         robot_state_publisher,
         # spot_pointcloud2,
         webots_event_handler,
-        Node(
-            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', '/Spot/Velodyne_Puck/point_cloud'), ],
-            parameters=[{
-                'transform_tolerance': 0.01,
-                'min_height': 0.0,
-                'max_height': 1.0,
-                'angle_min': -3.14,
-                'angle_max': 3.14,
-                'angle_increment': 0.00872,
-                'scan_time': 0.1,
-                'range_min': 0.9,
-                'range_max': 100.0,
-                'use_inf': True,
-                'inf_epsilon': 1.0,
-            }],
-            name='pointcloud_to_laserscan'
-        ),
-        reset_handler
-    ] + get_ros2_nodes())
+        pointcloud_to_laserscan_node
+    ])
