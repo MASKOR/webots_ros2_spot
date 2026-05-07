48706ab231d9ef7de31652710977a2ebc7ddb505
Ninad Kulkarni <ninadkulkarni21052000@gmail.com>
2026-05-07 16:51:47 +0200

    add sim time parameter

--- launch/dexboard_launch.py
+++ launch/dexboard_launch.py
@@ -1,9 +1,11 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 
 def generate_launch_description():
     return LaunchDescription(
         [
+            SetParameter(name="use_sim_time", value=True),
+
             # Linear Center (Reference)
             # Node(
             #     package="tf2_ros",
